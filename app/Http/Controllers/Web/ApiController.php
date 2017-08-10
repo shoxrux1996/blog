05703996178817e6894b89694c3f177b2bcb2e9f
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-08-10 15:54:24 +0500

    New commit

--- app/Http/Controllers/Web/ApiController.php
+++ app/Http/Controllers/Web/ApiController.php
@@ -1,32 +1,43 @@
 <?php
 
 namespace yuridik\Http\Controllers\Web;
+
 use Illuminate\Http\Response;
 use yuridik\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
-use yuridik\Paycom\Application;
+use yuridik\Transaction;
 
 
 class ApiController extends Controller
 {
-
-    
-    public function show(){
+    public function show()
+    {
         $merchant_id = \Config::get('paycom');
 
-        $user = "1233";
-        $amount = 400000;
 
-        return view('card.checkout')->withMerchant_id($merchant_id['merchant_id'])->withUser($user)->withAmount($amount);
+        if (Auth::guard('client')->check()) {
+            $id = Auth::guard('client')->user()->user->id;
+            $transactions = Transaction::where('user_id', $id)->where('');
+        }
+        elseif(Auth::guard('lawyer')->check()
+        ){
+            $id = Auth::guard('lawyer')->user()->user->id;
+        }
+        else
+            return redirect()->route('user.login');
+
+        return view('card.checkout')->withMerchant_id($merchant_id['merchant_id'])->withUser_id($id);
     }
-    public function getApi(Request $request){
-      /*$paycomConfig = \Config::get('paycom');
+
+    public function getApi(Request $request)
+    {
+        /*$paycomConfig = \Config::get('paycom');
 
 
 
-        echo json_encode($response);
-        $application = new Application($paycomConfig);
-        $application->run();*/
+          echo json_encode($response);
+          $application = new Application($paycomConfig);
+          $application->run();*/
     }
 }
--- resources/views/card/checkout.blade.php
+++ resources/views/card/checkout.blade.php
@@ -1,26 +1,69 @@
 @extends('layouts.app')
+@section('styles')
 
-@section('title', "| Insert Category ")
-
-
+@endsection
+@section('body')
+    @extends('layouts.body')
+@section('menu')
+    <li><a href="{{ route('home')}}">Главная</a></li>
+    <li><a href="{{ route('lawyers.list')}}">Юристы</a></li>
+    <li><a href="{{ route('question.list')}}">Вопросы</a></li>
+    <li><a href="{{ route('web.blogs')}}">Блог</a></li>
+    <li><a href="{{ route('how-works')}}">Как это работает</a></li>
+    <li><a href="{{ route('about')}}">О нас</a></li>
+@endsection
 @section('content')
-    <div class="row">
-        <form method="POST" action="http://checkout.test.paycom.uz">
-            <input type="text" name="merchant" value={{ $merchant_id }}>
-            <input type="text" name="amount" value="4000" >
-            <input type="text" name="account[user_id]" value={{$user}} >
+    <div class="container">
+        <div class="row" style="margin-top: 50px;">
+            <div class="col-lg-4 col-md-6">
+                <select id="paymentSelector" class="form-control">
+                    <option value="0" selected="selected">Выберите способ оплаты</option>
+                    <option value="paymeForm">Payme</option>
+                    <option value="upayForm">Upay</option>
+                    <option value="clickForm">Click</option>
+                    <option value="mbankForm">MBANK</option>
+                </select>
+                <div id="paymeForm">
+                    <form method="POST" action="http://checkout.test.paycom.uz">
+                        <input type="hidden" name="merchant" value={{ $merchant_id }}>
+                        <input type="hidden" name="amount" class="payment-amount payment-amount-payme">
+                        <input type="hidden" name="account[user_id]" value={{$user_id}} >
+                        <input type="number" id="amountWillConvert" class="form-control" placeholder="Введите сумму">
+                        <!-- ============= Не обязательные поля ====================== -->
+                        <input type="hidden" name="callback" value="http://yuridik.uz:443/success/:transaction">
+                    {{--<input type="hidden" name="callback" value="http://shoxrux19960822.000webhostapp.com/card"/>--}}
+                    <!-- ================================================== -->
+                        <button class="btn btn-primary">Перейти к оплате</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <section id="payment-payment" class="section-group">
+            <h2 class="header">Транзакции</h2>
+            <td class="list-view grid" id="payment-grid">
+                <table class="table table-striped">
+                    <tr>
+                        <th><a href="#">ID</a></th>
+                        <th><a href="#">Название сервиса</a></th>
+                        <th><a href="#">Время</a></th>
+                        <th><a href="#">Сумма</a></th>
+                    </tr>
+                    <tr>
+                        <td class="td">#365087</td>
+                        <td class="td">http://player.uz - Месячная подписка PLAYER FULL</td>
+                        <td class="td">2017-04-08 19:52:02</td>
+                        <td class="td"><span class="sign">-</span> 5,000.00</td>
+                    </tr>
+                </table>
 
-            
-
-            <!-- ============= Не обязательные поля ====================== -->
-            <input type="hidden" name="callback" value="http://shoxrux19960822.000webhostapp.com/success/:transaction">
-
-            {{--<input type="hidden" name="callback" value="http://shoxrux19960822.000webhostapp.com/card"/>--}}
-            <!-- ================================================== -->
-
-            <button type="submit">Оплатить с помощью <b>PAYCOM</b></button>
-        </form>
-
-
+        </section>
     </div>
+@endsection
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        $('#paymeForm form').submit(function (e) {
+            $('[name="amount"]').val($('#amountWillConvert').val() * 100)
+        })
+    </script>
 @endsection
