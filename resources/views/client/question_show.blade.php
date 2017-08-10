fb1453c01bd8cf39a08376b9cbaad9664b11c6fa
Erkin <erkinboy.botirov@mail.ru>
2017-08-10 13:08:55 +0500

    New Commit

---
+++ resources/views/client/question_show.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.app')
+@section('styles')
+    <link href="{{ asset('dist/css/questions.css')}}" rel="stylesheet">
+@endsection
+@section('body')
+    @extends('layouts.body')
+@section('menu')
+    <li><a href="{{ route('home')}}">Главная</a></li>
+    <li><a href="{{ route('lawyers.list')}}">Юристы</a></li>
+    <li><a href="{{ route('question.list')}}">Вопросы</a></li>
+    <li><a href="{{ route('web.blogs')}}">Блог</a></li>
+    <li><a href="{{ route('how-works')}}">Как это работает</a></li>
+    <li><a href="{{ route('about')}}">О нас</a></li>
+@endsection
+@section('content')
+    <div id="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="col-sm-12 question">
+                        @if($question->type ==2)
+                            <span class="question-price">
+                            <b>{{$question->price}} сум</b>
+                            <span>
+                            стоимость<br/>
+                            вопроса
+                            </span>
+                        </span>
+                        @endif
+                        <h4 class="title">{{$question->title}}</h4>
+                        <p class="description">{{$question->text}}</p>
+                        <p>
+                            <span class="date">{{Carbon\Carbon::parse($question->created_at)->toFormattedDateString()}}</span>
+                            <span class="number"> вопрос №{{$question->id}}</span>
+                            <span class="author">{{$question->client->user->firstName}}
+                                , г.{{$question->client->user->city->name}} </span>
+                        </p>
+                        <hr>
+                        <p>
+                            <span class="category">Категория: <a href="">{{$question->category->name}}</a></span>
+                            <i class="answers">
+                                {{$question->answers->count()}}
+                            </i>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <h3>Answers</h3>
+                @foreach($question->answers as $answer)
+                    <div class="panel panel-content col-sm-9">
+                        <div class="comment">
+                            <div class="author-info">
+                                <div class="author-name col-sm-9">
+                                    <a href="#"><h4>{{$answer->lawyer->user->firstName}}</h4></a>
+                                    <div class="comment-content">
+                                        {{$answer->text}}
+                                    </div>
+                                    <div class="tags col-md-6">
+                                        @foreach($answer->files as $file)
+                                            <a class="label label-default"
+                                               href={!!asset(rawurlencode($file->path.$file->file))!!}> {{ $file->file}}</a>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                <div>
+                                    <p class="author-time">{{date('F n, Y - g:iA'), strtotime($answer->created_at)}}</p>
+                                    <img src="{{ $answer->lawyer->file != null ? asset($answer->file->path.$answer->file->file) : asset('dist/images/avatar_large_male_client_default.jpg')}}"
+                                         class="author-img col-md-3" style=" width: 200px; height: 200px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            @if(Auth::guard('client')->check() && ($answer->feedback == null))
+
+                                {{Form::open(['route' => ['feedback.create', $answer->id],'method' => 'POST']) }}
+
+                                <div class="col-md-5">
+
+                                    {{Form::label('text', "Отзыв: ") }}
+                                    {{Form::textarea('text', null, ['rows' =>'2', 'cols'=>'43'])}}
+                                    <div class="col-md-12">
+                                        <div class="col-md-6">like {{Form::radio('helped',true,true)}}</div>
+                                        <div class="col-md-4 " style="float:right;">
+                                            dislike {{Form::radio('helped',false)}}</div>
+                                    </div>
+                                    {{Form::submit('Оставить отзыв', ['class'=> 'btn btn-success btn-block'])}}
+                                </div>
+
+                                {{Form::close() }}
+                            @endif
+                            @if($answer->feedback != null)
+                                <p style="color:rebeccapurple">{{$answer->feedback->text}}</p>
+                            @endif
+                        </div>
+                    </div>
+
+                @endforeach
+            </div>
+            <div class="row">
+                @if (Auth::guard('lawyer')->check() && $question->solved != true)
+                    @if($question->type == 2  && Auth::guard('lawyer')->user()->type == 2)
+
+                        {{Form::open(['route' => ['lawyer.answer.store', $question->id],'enctype' => 'multipart/form-data', 'method' => 'POST']) }}
+
+                        <div class="panel panel-danger col-md-10" style="padding-top: 10px;">
+                            @if ($errors->has('text'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('text') }}</strong>
+                                    </span>
+                            @endif
+                            <div>
+                                {{Form::textarea('text', null,['style' =>'width:100%;', 'placeholder'=>'Ответить'])}}
+                            </div>
+
+
+                            <div class="col-md-4" style="margin:10px;">
+                                {{Form::file('files[]', ['multiple' => 'multiple'])}}
+                            </div>
+                            <div class="col-md-2" style="float:right; margin: 10px;">
+                                {{Form::submit('Ответить', ['class'=> 'btn btn-success'])}}
+                            </div>
+
+                        </div>
+                        {{Form::close() }}
+
+                    @endif
+                    @if($question->type == 1)
+                        <div class="row">
+                            {{Form::open(['route' => ['lawyer.answer.store', $question->id],'enctype' => 'multipart/form-data', 'method' => 'POST']) }}
+
+                            <div class="panel panel-danger col-md-10" style="padding-top: 10px;">
+                                @if ($errors->has('text'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('text') }}</strong>
+                                    </span>
+                                @endif
+                                <div>
+                                    {{Form::textarea('text', null,['style' =>'width:100%;', 'placeholder'=>'Ответить'])}}
+                                </div>
+
+
+                                <div class="col-md-4" style="margin:10px;">
+                                    {{Form::file('files[]', ['multiple' => 'multiple'])}}
+                                </div>
+                                <div class="col-md-2" style="float:right; margin: 10px;">
+                                    {{Form::submit('Ответить', ['class'=> 'btn btn-success'])}}
+                                </div>
+
+                            </div>
+                            {{Form::close() }}
+                        </div>
+                    @endif
+
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
+@endsection
+@section('scripts')
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=ha04cxa9mauwibgqmd91jvlug5qd3gqfb1ihnf8s5imb73na"></script>
+
+    <script>tinymce.init({
+            selector: 'textarea',
+            plugins: 'link code',
+            height: 500,
+            toolbar: 'undo redo | cut copy paste'
+        });</script>
+@endsection
---
+++ resources/views/client/questions.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+@section('styles')
+<link href="{{ asset('dist/css/questions.css')}}" rel="stylesheet">
+@endsection
+@section('body')
+@extends('layouts.body')
+@section('menu')
+  <li><a href="{{ route('home')}}">Главная</a></li>
+  <li><a href="{{ route('lawyers.list')}}">Юристы</a></li>
+  <li><a href="{{ route('question.list')}}">Вопросы</a></li>
+  <li><a href="{{ route('web.blogs')}}">Блог</a></li>
+  <li><a href="{{ route('how-works')}}">Как это работает</a></li>
+  <li><a href="{{ route('about')}}">О нас</a></li>
+@endsection
+@section('content')
+
+
+	<div id="wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9">
+
+                
+                @foreach($questions as $question)
+
+                <div class="col-sm-12 question">
+                    @if($question->type ==2)
+                        <span class="question-price">
+                        <b>{{$question->price}} сум</b>
+                        <span>
+                            стоимость<br />
+                            вопроса
+                        </span>
+                    </span>
+                    @endif
+                    <h4 class="title"><a href="{{ route('client.question.show', $question->id)}}">{{$question->title}}</a></h4>
+                    <p class="description">{{$question->text}}</p>
+                    <p>
+                        <span class="date">{{Carbon\Carbon::parse($question->created_at)->toFormattedDateString()}}</span>
+                        <span class="number"> вопрос №{{$question->id}}</span>
+                        <span class="author">{{$question->client->user->firstName}}, г.{{$question->client->user->city->name}} </span>
+                    </p>
+                    <hr>
+                    <p>
+                        <span class="category">Категория: <a href="">{{$question->category->name}}</a></span>
+                        <a class="answers" href="{{route('web.question.show', $question->id)}}">
+                            {{$question->answers->count()}}
+                        </a>
+                    </p>
+                </div>
+                @endforeach
+
+                <div class="col-sm-12 text-center">
+                    {!! $questions->links('pagination') !!}
+
+                </div>
+            </div>
+            <div class="col-sm-3 text-center">
+                <h3>Лучшие юристы</h3>
+                <div class="best-lawyers">
+                    <img src="{{ asset('dist/images/headshot-1.png')}}" class="img-rounded" />
+                    <h3>Керс Олег</h3>
+                    <h6>
+                        <b>юрист, г. Калининград</b>
+                    </h6>
+                    <a type="button" class="btn btn-default btn-success" href="individual-lawyer.html">Посмотреть профиль</a>
+                </div>
+                <div class="best-lawyers">
+                    <img src="{{ asset('dist/images/headshot-1.png')}}" class="img-rounded" />
+                    <h3>Керс Олег</h3>
+                    <h6>
+                        <b>юрист, г. Калининград</b>
+                    </h6>
+                    <a type="button" class="btn btn-default btn-success" href="individual-lawyer.html">Посмотреть профиль</a>
+                </div>
+                <div class="best-lawyers">
+                    <img src="{{ asset('dist/images/headshot-1.png')}}" class="img-rounded" />
+                    <h3>Керс Олег</h3>
+                    <h6>
+                        <b>юрист, г. Калининград</b>
+                    </h6>
+                    <a type="button" class="btn btn-default btn-success" href="individual-lawyer.html">Посмотреть профиль</a>
+                </div>
+                <div class="ask-question-block text-center">
+                    <img class="img-responsive" src="{{ asset('dist/images/one-word-save_0')}}.png" />
+                    <h6>
+                        <b>Задайте вопрос бесплатно</b>
+                    </h6>
+                    <a class="btn btn-default btn-success pulse-button" type="button" href="ask-question.html">Задать вопрос</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@endsection
