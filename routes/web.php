5d01520fa7dab9a588026054230e9c1143922094
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-08-11 16:16:36 +0500

    New commit

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,6 @@
 <?php
-/*Route::get('payment', function(){
 
-    $question = yuridik\Question::find(42);
-    $order = new yuridik\Order;
-    $order->user_id = $question->client->user->id;
-    $order->amount = $question->price;
-    
-
-    $question->order()->save($order);
-
-});*/
 Route::get('card','Web\ApiController@show')->name('card.payment');
-
-/*Route::get('balance', function(){
-    $client = Auth::guard('client')->user();
-    foreach ($client->user->transactions->where('state',2) as $key) {
-        echo 'Transactions: '.$key->amount .'<br>';
-    }
-    foreach ($client->user->orders as $key) {
-        echo 'Orders: '.$key->amount .'<br>';
-    }
-    echo 'Balance: <strong>'.$client->user->balance() .'</strong>';
-    
-});*/
-
 
 Route::prefix('admin')->group(function(){
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
