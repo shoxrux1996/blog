08c53f9bfd3e9d30a7f618aea202c8a8c0b8ec93
ulugbek <samigjonov@gmail.com>
2017-08-18 12:05:27 +0500

    Refer to lawyer modal added

--- resources/views/lawyer/show.blade.php
+++ resources/views/lawyer/show.blade.php
@@ -37,7 +37,30 @@
                         <!-- END SIDEBAR USER TITLE -->
                         <!-- SIDEBAR BUTTONS -->
                         <div class="profile-userbuttons">
-                            <button type="button" class="btn btn-success btn-sm">Обратиться к юристу</button>
+                            <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#refer-lawyer">Обратиться к юристу</button>
+
+                            <!-- Modal for refer lawyer function-->
+                            <div id="refer-lawyer" class="modal fade" role="dialog">
+                                <div class="modal-dialog">
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">@lang('index.soon')!!!</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img src="{{asset('dist/images/under-development.png')}}" alt="Under development"/>
+                                            <h4>Функция "обратиться к юристу" в процессе разработки</h4>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default btn-dark-blue" data-dismiss="modal">@lang('index.close')</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- /Modal for refer lawyer function-->
+
                         </div>
                         <!-- END SIDEBAR BUTTONS -->
                         <!-- SIDEBAR MENU -->
