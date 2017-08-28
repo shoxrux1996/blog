0511d12fcf8f956112e9c750b80704c7dad1d826
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-08-28 18:41:57 +0500

    error in category controller

--- resources/views/email/verify.blade.php
+++ resources/views/email/verify.blade.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
-        <h2>{{ __('email.email_confirm') }}</h2>
-
+        <h2></h2>
         <div>
-            {{ __('email.email_confirm_div') }}
             <a class="btn btn-default btn-lg" href="{{route('user.register.confirm', ['code' => $data['code']]) }}">
-                {{ __('email.confirm_email_button') }}</a><br/>
+                Xa</a><br/>
         </div>
 
     </body>
