557c6bd0b6ae72106079f310d85326d7030d1fec
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-09-02 13:41:01 +0500

    comment faqat lawyer type == 2, savoldayam wunaqa (hozirca)

--- resources/views/blog/blog_show.blade.php
+++ resources/views/blog/blog_show.blade.php
@@ -19,32 +19,27 @@
                 <span>Facebook</span>
             </a>
         </li>
-
         <li>
             <a class="a fa-odnoklassniki-square" href="https://ok.ru">
                 <span>Odnoklassniki</span>
             </a>
         </li>
-
         <li>
             <a class="fa fa-telegram" href="https://www.telegram.me/">
                 <span>Telegram</span>
             </a>
         </li>
-
         <li>
             <a class="fa fa-twitter" href="https://www.twitter.com">
                 <span>Twitter</span>
             </a>
         </li>
-
     </ul>
     <!-- /Social -->
 
 
     <!-- Content -->
     <div class="container-fluid">
-
         <!-- Blog item title -->
         <div class="row">
             <div class="blog-title text-center"
@@ -57,7 +52,8 @@
                 </h1>
 
                 <p>
-                <h6>Автор: {{$blog->blogable->user != null ? $blog->blogable->user->firstName : $blog->blogable->name}}</h6>
+                <h6>
+                    Автор: {{$blog->blogable->user != null ? $blog->blogable->user->firstName : $blog->blogable->name}}</h6>
                 @php
                     $time= Carbon\Carbon::parse($blog->created_at)->toFormattedDateString() ;
                 @endphp
@@ -66,16 +62,13 @@
             </div>
         </div>
         <!-- /Blog item title -->
-
         <!-- Blog item text -->
         <div class="row padding-30">
             <div class="col-sm-2 advertisement left-sidebar">
                 <img class="img-responsive center-block" src="{{asset('dist/images/sidebar-ad.png')}}"/>
                 <img class="img-responsive center-block" src="{{asset('dist/images/Sidebar-ad1.jpg')}}"/>
-
             </div>
             <div class="col-sm-8 blog-text-description">
-
                 {!! $blog->text !!}
                 <div>
                     <label>Tags: </label>
@@ -84,7 +77,6 @@
                     @endforeach
                 </div>
             </div>
-
             <div class="col-sm-2 right-sidebar advertisement">
                 <div class="ask-question-block">
                     <img class="img-responsive" src="{{asset('dist/images/one-word-save_0.png')}}"/>
@@ -101,7 +93,6 @@
             </div>
         </div>
         <!-- /Blog item text -->
-
         <!-- Blog comments-->
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -109,116 +100,119 @@
                 <h3><span class="">{{$blog->comments->count()}}</span> комментарии</h3>
                 <div class="row">
                     <div class="comments-container">
-
                         <ul id="comments-list" class="comments-list">
                             @foreach($blog->comments as $comment)
-                            <li>
-                                <div class="comment-main-level">
-                                    <!-- Avatar -->
-                                    <div class="comment-avatar"><img src="{{$comment->commentable->user->file != null
+                                <li>
+                                    <div class="comment-main-level">
+                                        <!-- Avatar -->
+                                        <div class="comment-avatar"><img src="{{$comment->commentable->user->file != null
                                      ? asset($comment->commentable->user->file->path.$comment->commentable->user->file->file)
                                      : asset("dist/images/headshot-2.jpg")}}" alt=""></div>
-                                    <div class="comment-box">
-                                        <div class="comment-head">
-                                            <h6 class="comment-name"><a href="{{route('web.lawyer.show', $comment->commentable->id)}}">{{$comment->commentable->user->firstName}}</a></h6>
-                                            @php
-                                                $end = \Carbon\Carbon::parse($comment->created_at);
-                                                $now = \Carbon\Carbon::now();
-                                            @endphp
-                                            <span>{{$end->diffForHumans($now)}}</span>
-                                        </div>
-                                        <div class="comment-content">
-                                            {{$comment->comment}}
+                                        <div class="comment-box">
+                                            <div class="comment-head">
+                                                <h6 class="comment-name"><a
+                                                            href="{{route('web.lawyer.show', $comment->commentable->id)}}">{{$comment->commentable->user->firstName}}</a>
+                                                </h6>
+                                                @php
+                                                    $end = \Carbon\Carbon::parse($comment->created_at);
+                                                    $now = \Carbon\Carbon::now();
+                                                @endphp
+                                                <span>{{$end->diffForHumans($now)}}</span>
+                                            </div>
+                                            <div class="comment-content">
+                                                {{$comment->comment}}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </li>
+                                </li>
                             @endforeach
                         </ul>
                     </div>
                 </div>
                 <!-- /Comments -->
-
                 <!-- Leave your comment -->
-                @if(Auth::guard('lawyer')->check())
-                <div class="row">
-                    <h3>Оставь свою комментарию</h3>
-                    <div class="widget-area no-padding blank">
-                        <div class="status-upload">
-                            <form method="post" action="{{route('lawyer.comment.store', $blog->id)}}">
-                                {{csrf_field()}}
-                                <textarea name="comment" placeholder="Ваше сообщение" ></textarea>
-                                <button type="submit" class="btn btn-success green"><i class="fa fa-share"></i> Опубликовать</button>
-                            </form>
-                        </div><!-- Status Upload  -->
-                    </div><!-- Widget Area -->
-                </div>
-                @endif
+                    @if(Auth::guard('lawyer')->check() && Auth::guard('lawyer')->user()->type == 2)
+                        <div class="row">
+                            <h3>Оставь свою комментарию</h3>
+                            <div class="widget-area no-padding blank">
+                                <div class="status-upload">
+                                    <form method="post" action="{{route('lawyer.comment.store', $blog->id)}}">
+                                        {{csrf_field()}}
+                                        <textarea name="comment" placeholder="Ваше сообщение"></textarea>
+                                        <button type="submit" class="btn btn-success green"><i class="fa fa-share"></i>
+                                            Опубликовать
+                                        </button>
+                                    </form>
+                                </div><!-- Status Upload  -->
+                            </div><!-- Widget Area -->
+                        </div>
+                    @endif
                 <!-- /Leave your comment -->
-
                 <hr>
             </div>
         </div>
         <!-- /Blog comments-->
 
-                <!-- Subscribe -->
-                <div class="row subscribe">
-                    <div class="padding-30 text-center">
-                        <h2>Нравится журнал? Подпишись!</h2>
-                        <h4>Самое интересное 2 раза в месяц на ваш e-mail</h4>
-                        <div class="col-sm-offset-3 col-sm-4">
-                            <div class="form-group">
-                                <input type="text" class="form-control general-input" placeholder="Введите ваш email"/>
-                            </div>
-                        </div>
-                        <div class="col-sm-1">
-                            <button type="button" class="btn btn-default dark-blue">Подписаться</button>
-                        </div>
+        <!-- Subscribe -->
+        <div class="row subscribe">
+            <div class="padding-30 text-center">
+                <h2>Нравится журнал? Подпишись!</h2>
+                <h4>Самое интересное 2 раза в месяц на ваш e-mail</h4>
+                <div class="col-sm-offset-3 col-sm-4">
+                    <div class="form-group">
+                        <input type="text" class="form-control general-input" placeholder="Введите ваш email"/>
                     </div>
                 </div>
-                <!-- /Subcribe -->
-                <div class="row padding-30" id="other-articles">
-                    <h3 class="text-center">Читайте также</h3>
-                    @foreach($blogs as $bl)
-                        <a href="{{route('web.blog.show', $bl->id)}}">
-                            <div class="col-sm-4">
-                                <div class="blog-item">
-                                    <div class="ribbon"><span>{{$blog->blogable_type != 'yuridik\Admin' ? 'Юрист' : 'Модератор'}}</span></div>
-                                    <div class="blog-item-img">
-                                        <img alt="Blog item image" src="{{$bl->file != null ? asset($bl->file->path.$bl->file->file) : asset('dist/images/blog-img-2.jpg')}}"/>
-                                        <div class="middle">
-                                            <a href="{{route('web.blog.show', $bl->id)}}" class="btn btn-dark-blue text">Читать
-                                                статью</a>
-                                        </div>
-                                    </div>
-                                    <div class="blog-item-description">
-                                        <h5><b>{{ $bl->title }}</b></h5>
-                                        <p>{{substr(strip_tags($bl->text),0,100)}} {{strlen(strip_tags($bl->text))>100 ? '...' : ""}}</p>
-                                        <p class="post-info">
+                <div class="col-sm-1">
+                    <button type="button" class="btn btn-default dark-blue">Подписаться</button>
+                </div>
+            </div>
+        </div>
+        <!-- /Subcribe -->
+        <div class="row padding-30" id="other-articles">
+            <h3 class="text-center">Читайте также</h3>
+            @foreach($blogs as $bl)
+                <a href="{{route('web.blog.show', $bl->id)}}">
+                    <div class="col-sm-4">
+                        <div class="blog-item">
+                            <div class="ribbon">
+                                <span>{{$blog->blogable_type != 'yuridik\Admin' ? 'Юрист' : 'Модератор'}}</span></div>
+                            <div class="blog-item-img">
+                                <img alt="Blog item image"
+                                     src="{{$bl->file != null ? asset($bl->file->path.$bl->file->file) : asset('dist/images/blog-img-2.jpg')}}"/>
+                                <div class="middle">
+                                    <a href="{{route('web.blog.show', $bl->id)}}" class="btn btn-dark-blue text">Читать
+                                        статью</a>
+                                </div>
+                            </div>
+                            <div class="blog-item-description">
+                                <h5><b>{{ $bl->title }}</b></h5>
+                                <p>{{substr(strip_tags($bl->text),0,100)}} {{strlen(strip_tags($bl->text))>100 ? '...' : ""}}</p>
+                                <p class="post-info">
                                 <span>
                                     <i class="fa fa-eye">{{$bl->count}}</i>
                                 </span>
                                     <span class="pull-right">
                                     <i class="fa fa-comments-o"></i> {{$bl->comments->count()}}
                                 </span>
-                                        </p>
-                                    </div>
-                                    <hr>
-                                    <div class="blog-item-footer">
+                                </p>
+                            </div>
+                            <hr>
+                            <div class="blog-item-footer">
                             <span>
                                 <i class="fa fa-user"></i> {{$blog->blogable->user != null ? $blog->blogable->user->firstName : $blog->blogable->name}}
                             </span>
                                 <span class="pull-right">
                                 <i class="fa fa-calendar"></i> {{Carbon\Carbon::parse($bl->created_at)->toFormattedDateString()}}
                             </span>
-                                    </div>
-                                </div>
                             </div>
-                        </a>
-                    @endforeach
+                        </div>
+                    </div>
+                </a>
+            @endforeach
 
-                </div>
-            </div>
-            <!-- /Content -->
+        </div>
+    </div>
+    <!-- /Content -->
 
 @endsection
