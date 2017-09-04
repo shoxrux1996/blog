b578f055d5504f2825429a0b41a9b2f84fddc593
ulugbek <samigjonov@gmail.com>
2017-09-04 20:01:50 +0500

    Admin-->users style changed

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -350,92 +350,109 @@
                             </div>
                             <div class="col-md-9 tab-pane fade profile-content" id="answers-{{$lawyer->id}}">
                                 <div class="col-sm-12">
+                                    <h4>Отвеченные вопросы</h4>
                                     @foreach($lawyer->questions() as $question)
-                                        <div class="col-sm-12 question">
-                                            @if($question->type == 2 || $question->type == 1)
-                                                <span class="question-price">
-                                        <b>{{$question->price}} сум</b>
-                                        <span>
-                                            стоимость<br/>
-                                            вопроса
-                                        </span>
-                                    </span>
-                                            @endif
-                                            <h4 class="title"><a
-                                                        href="{{route('web.question.show', $question->id)}}">{{$question->title}}</a>
-                                            </h4>
-                                            <p class="description">{{$question->text}}</p>
-                                            <p>
-                                                <span class="date">{{Carbon\Carbon::parse($question->created_at)->toFormattedDateString()}}</span>
-                                                <span class="number"> вопрос №{{$question->id}}</span>
-                                                <span class="author">{{$question->client->user->firstName}}
-                                                    , г.{{$question->client->user->city->name}} </span>
-                                            </p>
-                                            <hr>
-                                            <p>
-                                        <span class="category">Категория: <a
-                                                    href="{{route('web.category.show', $question->category->name)}}">{{$question->category->name}}</a></span>
-                                                <a class="answers"
-                                                   href="{{route('web.question.show', $question->id)}}">
-                                                    {{$question->answers->count()}}
+                                        <ul>
+                                            <li>
+                                                <a href="{{route('web.question.show', $question->id)}}">
+                                                    {{$question->title}}
+                                                    @if($question->type == 2 || $question->type == 1)
+                                                        <span class="badge">{{$question->price}} сум</span>
+                                                    @endif
                                                 </a>
-                                            </p>
-                                        </div>
+                                            </li>
+                                        </ul>
+                                        {{--<div class="col-sm-12 question">--}}
+                                            {{--@if($question->type == 2 || $question->type == 1)--}}
+                                                {{--<span class="question-price">--}}
+                                        {{--<b>{{$question->price}} сум</b>--}}
+                                        {{--<span>--}}
+                                            {{--стоимость<br/>--}}
+                                            {{--вопроса--}}
+                                        {{--</span>--}}
+                                    {{--</span>--}}
+                                            {{--@endif--}}
+                                            {{--<h4 class="title"><a--}}
+                                                        {{--href="{{route('web.question.show', $question->id)}}">{{$question->title}}</a>--}}
+                                            {{--</h4>--}}
+                                            {{--<p class="description">{{$question->text}}</p>--}}
+                                            {{--<p>--}}
+                                                {{--<span class="date">{{Carbon\Carbon::parse($question->created_at)->toFormattedDateString()}}</span>--}}
+                                                {{--<span class="number"> вопрос №{{$question->id}}</span>--}}
+                                                {{--<span class="author">{{$question->client->user->firstName}}--}}
+                                                    {{--, г.{{$question->client->user->city->name}} </span>--}}
+                                            {{--</p>--}}
+                                            {{--<hr>--}}
+                                            {{--<p>--}}
+                                        {{--<span class="category">Категория: <a--}}
+                                                    {{--href="{{route('web.category.show', $question->category->name)}}">{{$question->category->name}}</a></span>--}}
+                                                {{--<a class="answers"--}}
+                                                   {{--href="{{route('web.question.show', $question->id)}}">--}}
+                                                    {{--{{$question->answers->count()}}--}}
+                                                {{--</a>--}}
+                                            {{--</p>--}}
+                                        {{--</div>--}}
                                     @endforeach
                                 </div>
                             </div>
 
                             <div class="col-md-9 tab-pane fade profile-content" id="articles-{{$lawyer->id}}">
                                 <div class="row">
+                                    <h4>Написанные статьи</h4>
                                     @foreach($lawyer->blogs as $blog)
-                                        <a href="{{route('web.blog.show', $blog->id)}}">
-                                            <div class="col-sm-6">
-                                                <div class="blog-item">
-                                                    <div class="ribbon">
-                                                        <span>{{$blog->blogable_type != 'yuridik\Admin' ? 'Юрист' : 'Модератор'}}</span>
-                                                    </div>
-                                                    <div class="blog-item-img">
-                                                        @if($blog->file != null)
-                                                            <img alt="Blog item image"
-                                                                 src="{{asset($blog->file->path.$blog->file->file)}}">
-                                                        @else
-                                                            <img alt="Blog item image"
-                                                                 src="{{asset('dist/images/blog-img-2.jpg')}}">
-                                                        @endif
-                                                        <div class="middle">
-                                                            <button class="btn btn-dark-blue text">Читать статью
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                    <div class="blog-item-description">
-                                                        <h5>
-                                                            <b>{{substr($blog->title,0,70)}} {{strlen($blog->title)>70 ? '...' : ""}}</b>
-                                                        </h5>
-                                                        <p>{{substr(strip_tags($blog->text),0,180)}} {{strlen(strip_tags($blog->text))>180 ? '...' : ""}}</p>
-                                                        <p class="post-info">
-                                            <span>
-                                                <i class="fa fa-eye"></i> {{$blog->count}}
-                                            </span>
-                                                            @foreach($blog->tags as $tag)
-                                                                <span style="margin-left:10px;"><strong>{{$tag->name}}</strong></span>
-                                                            @endforeach
-                                                            <span class="pull-right">
-                                                <i class="fa fa-comments-o"></i> {{$blog->comments->count()}}
-                                            </span>
-                                                        </p>
-                                                    </div>
-                                                    <hr>
-                                                    <div class="blog-item-footer">
-                                            <span>
-                                                <i class="fa fa-user"></i> {{$blog->blogable->user != null ? $blog->blogable->user->firstName : $blog->blogable->name}}
-                                            </span>
-                                                        <span class="pull-right">
-                                            <i class="fa fa-calendar"></i> {{Carbon\Carbon::parse($blog->created_at)->toFormattedDateString()}}
-                                            </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
+                                        <ul>
+                                            <li>
+                                                <a href="{{route('web.blog.show', $blog->id)}}">{{$blog->title}}</a>
+                                            </li>
+                                        </ul>
+                                        {{--<a href="{{route('web.blog.show', $blog->id)}}">--}}
+                                            {{--<div class="col-sm-6">--}}
+                                                {{--<div class="blog-item">--}}
+                                                    {{--<div class="ribbon">--}}
+                                                        {{--<span>{{$blog->blogable_type != 'yuridik\Admin' ? 'Юрист' : 'Модератор'}}</span>--}}
+                                                    {{--</div>--}}
+                                                    {{--<div class="blog-item-img">--}}
+                                                        {{--@if($blog->file != null)--}}
+                                                            {{--<img alt="Blog item image"--}}
+                                                                 {{--src="{{asset($blog->file->path.$blog->file->file)}}">--}}
+                                                        {{--@else--}}
+                                                            {{--<img alt="Blog item image"--}}
+                                                                 {{--src="{{asset('dist/images/blog-img-2.jpg')}}">--}}
+                                                        {{--@endif--}}
+                                                        {{--<div class="middle">--}}
+                                                            {{--<button class="btn btn-dark-blue text">Читать статью--}}
+                                                            {{--</button>--}}
+                                                        {{--</div>--}}
+                                                    {{--</div>--}}
+                                                    {{--<div class="blog-item-description">--}}
+                                                        {{--<h5>--}}
+                                                            {{--<b>{{substr($blog->title,0,70)}} {{strlen($blog->title)>70 ? '...' : ""}}</b>--}}
+                                                        {{--</h5>--}}
+                                                        {{--<p>{{substr(strip_tags($blog->text),0,180)}} {{strlen(strip_tags($blog->text))>180 ? '...' : ""}}</p>--}}
+                                                        {{--<p class="post-info">--}}
+                                            {{--<span>--}}
+                                                {{--<i class="fa fa-eye"></i> {{$blog->count}}--}}
+                                            {{--</span>--}}
+                                                            {{--@foreach($blog->tags as $tag)--}}
+                                                                {{--<span style="margin-left:10px;"><strong>{{$tag->name}}</strong></span>--}}
+                                                            {{--@endforeach--}}
+                                                            {{--<span class="pull-right">--}}
+                                                {{--<i class="fa fa-comments-o"></i> {{$blog->comments->count()}}--}}
+                                            {{--</span>--}}
+                                                        {{--</p>--}}
+                                                    {{--</div>--}}
+                                                    {{--<hr>--}}
+                                                    {{--<div class="blog-item-footer">--}}
+                                            {{--<span>--}}
+                                                {{--<i class="fa fa-user"></i> {{$blog->blogable->user != null ? $blog->blogable->user->firstName : $blog->blogable->name}}--}}
+                                            {{--</span>--}}
+                                                        {{--<span class="pull-right">--}}
+                                            {{--<i class="fa fa-calendar"></i> {{Carbon\Carbon::parse($blog->created_at)->toFormattedDateString()}}--}}
+                                            {{--</span>--}}
+                                                    {{--</div>--}}
+                                                {{--</div>--}}
+                                            {{--</div>--}}
+                                        {{--</a>--}}
                                     @endforeach
 
                                 </div>
