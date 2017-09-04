b47c33e1f3d0b1128fa600adc5f81f182e85bedd
Erkin <erkinboy.botirov@mail.ru>
2017-09-04 12:28:11 +0500

    about translation

---
+++ resources/lang/uz/about.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erkin
+ * Date: 04.09.2017
+ * Time: 12:00
+ */
+return [
+    'Главная' => "Bosh sahifa",
+    'Юристы' => "Yuristlar",
+    'Вопросы' => "Savollar",
+    'Блог' => "Maqola",
+    'Как это работает' => "Bu qanday ishlaydi",
+    'О нас' => "Jamoa haqida",
+	'ЮРИДИЧЕСКИЕ КОНСУЛЬТАЦИИ ОНЛАЙН' => "ONLAYN HUQUQIY KONSULTATSIYA",
+	'это сервис, связывающий профессиональных юристов с клиентами в режиме онлайн. Любой пользователь может получить здесь моментальную консультацию по всем правовым вопросам, а практикующие юристы - возможность для дополнительного заработка.' => "professional huquqshunoslar va mijozlar/fuqarolarni bir-biri bilan onlayn tarzda bog‘lovchi xizmat turi hisoblanadi. Ushbu sayt har qanday foydalanuvchiga istalgan huquq sohasiga oid savollar yuzasidan tez fursatda va sifatli konsultatsiya olish imkonini, huquqshunoslarga esa huquqiy konsultatsiya berish orqali qo‘shimcha daromad topish imkoniyatini beradi.",
+	'БЫСТРО' => "TEZ",
+	'решение вашего вопроса в течение 2-х часов' => "savollarga 2 soat ichida javob olish",
+	'ДОСТУПНО' => "HAMYONBOP ",
+	'консультация юриста от 5000 сум' => "5000 so‘mdan boshlanuvchi huquqiy konsultatsiya",
+	'ПРАВИЛЬНО' => "ENG TO‘G‘RI ",
+	'коллегиальное мнение юристов' => "huquqshunoslarning kollegial fikri va xulosasini olish imkoniyati",
+	'ЗАРЕГИСТРИРОВАТЬСЯ' => "ЗАРЕГИСТРИРОВАТЬСЯ",
+	'для клиента' => "mijozlari uchun",
+	'для юриста' => "huquqshunoslari uchun",
+	'ВОПРОС ЮРИСТАМ' => "HUQUQSHUNOSLARGA SAVOL",
+	'С вашим вопросом ознакомятся практически все юристы сервиса. Это повышает не только скорость получения ответа на ваш запрос, но и качество консультации. Не исключено, что ответят на ваш вопрос сразу несколько юристов. Узнать больше' => "Savolingiz bilan saytdagi barcha huquqshunoslar tanishib chiqadi. Bu esa, faqatgina tez fursatda javob olishni emas, sifatli javob olishni ham ta’minlashga xizmat qiladi. Savolga bir vaqtning o‘zida bir nechta huquqshunos javob berishi ham mumkin.",
+	'Задать вопрос юристам' => "Savol berish",
+	'НОВЫЕ КЛИЕНТЫ' => "YANGI MIJOZLAR",
+	'Мы хотим, чтобы для вас не существовало никаких границ - ни территориальных, ни временных. Это означает, что в каком бы городе вы не находились, число ваших клиентов будет только увеличиваться. С Yuridik.uz у вас появляется реальный шанс наладить успешную практику, не выходя из собственного дома.' => "Biz siz uchun hech qanday chegara bo‘lmasligini xohlaymiz – hudud jihatidan ham, vaqt jihatidan ham. Bu shuni anglatadiki, Siz qayerda bo‘lsangiz ham, mijozlaringiz soni oshib boraveradi. Yuridik.uz bilan uyingizdan chiqmasdan turib muvaffaqiyatli faoliyat olib borish imkoniga ega bo‘lasiz.",
+	'ОНЛАЙН ЧАТ С ЮРИСТОМ' => "HUQUQSHUNOS BILAN ONLAYN SUHBAT",
+	'Бывают ситуации, когда для получения полноценной консультации клиенту необходим личный контакт с юристом. К вашим услугам - наш онлайн чат. Общайтесь в режиме диалога, выбрав соответствующие режимы (текст, звук, видео).' => "Shunday vaziyatlar bo‘ladiki, holat yuzasidan to‘liq konsultatsiya olish uchun huquqshunos bilan shaxsan bog‘lanishga to‘g‘ri keladi. Bunday vaziyatlarda onlayn suhbat bo‘limi - xizmatingizda! O‘zingizga qulay tarzda (matnli, ovozli, video) huquqshunos bilan shaxsan bog‘laning. ",
+	'Онлайн консультация' => "Onlayn konsultatsiya",
+	'ГИБКИЙ ГРАФИК И МЕСТО РАБОТЫ' => "O‘ZGARUVCHAN ISH VAQTI VA ISH OVRINLARI",
+	'Юристы Yuridik.uz не ограничены в своем рабочем времени. Работайте в наиболее комфортном для вас месте и в максимально удобном режиме. Мы же постараемся сделать так, чтобы таких возможностей у вас было как можно больше.' => "Yuridik.uz huquqshunoslari aniq belgilangan ish vaqti bilan chegaralab qo‘yilmaydi. O‘zingiz uchun eng qulay joyda va eng mos vaqt rejimida ishlang. Biz sizni iloji boricha ko‘proq imkoniyatlar bilan ta’minlashga harakat qilamiz.",
+	'ПОДГОТОВКА ДОКУМЕНТОВ' => "HUJJATLAR TAYYORLASH",
+	'Грамотно составить и оформить юридический документ неподготовленному человеку практически невозможно. Наши юристы оперативно сделают это за вас, даже в том случае, если потребуется создание уникального документа. Типовые формы правовых документов вы получите практически мгновенно.' => "Yetarli tayyorgarlik hamda tajribaga ega bo‘lmagan kishi uchun huquqiy hujjatlarni tayyorlash va rasmiylashtirish amaliy jihatdan deyarli imkonsiz hisoblanadi. Bizning huquqshunoslarimiz o‘ta noyob va murakkab huquqiy hujjatlarni Siz uchun tayyorlab beradi. Oddiy va odatiy hujjatlarni qisqa muddatda qo‘lga kiritishingiz mumkin.",
+	'Заказать документ' => "Hujjat buyurtma qilish",
+	'БОЛЬШОЙ ВЫБОР ВОПРОСОВ' => "SAVOLLAR TANLOVI",
+	'Практика показывает, что в силу своей профессии юристам приходится быть специалистом в различных отраслях. Вы можете заявить о себе, как о специалисте широкого профиля, так и узкой специализации. Общайтесь с коллегами, делитесь опытом, повышайте свой профессиональный уровень.' => "Amaliyot shuni ko‘rsatadiki, huquqshunoslik kasbi ko‘plab turli sohalarda mutaxassis bo‘lishni talab etadi. Siz o‘zingizni keng platformali mutaxassis yoki nisbatan tor – maxsuslashtirilgan sohada huquqshunos sifatida tanishtirishingiz mumkin. Hamkasblaringiz bilan muloqot o‘rnating, tajribangiz bilan bo‘lishing va o‘z navbatida, bilim va ko‘nikmalaringizni oshiring.",
+	'Вопросы' => "Savollar bazasi",
+	'ПОДБОР НУЖНОГО ЮРИСТА' => "KERAKLI HUQUQSHUNOSNI TANLASH",
+	'В каталоге Yuridik.uz - сотни высокопрофессиональных юристов. Определитесь с его специализацией, и наш сервис предложит вам список юристов, обладающих знаниями и опытом именно в данной области. Выбирайте и общайтесь!' => "Yuridik.uz katalogida yuzlab professional huquqshunoslar mavjud. Huquqshunosning sohasini tanlang va bizning xizmat sizga tanlangan soha bo‘yicha kerakli bilim va tajribaga ega bo‘lgan huquqshunoslar ro‘yxatini taqdim etadi. O‘zingiz istagan huquqshunosni tanlang va u bilan bog‘laning. ",
+	'Выбрать юриста' => "Huquqshunosni tanlash",
+	'РЕПУТАЦИЯ' => "REPUTATSIYA",
+	'Существует масса причин, объективно мешающих вашему профессиональному росту. Сотрудничество с Yuridik.uz кардинально меняет эту ситуацию. Участвуйте в соревновании со своими коллегами, зарабатывая рейтинг, репутацию, и как следствие, новых клиентов.' => "Huquqshunoslik faoliyatingizda professional o‘sish uchun bir qancha omil va sabablar mavjudligi hech kimga sir emas. Yuridik.uz bilan hamkorlik qilish ushbu holatni mutlaqo o‘zgartirish imkonini beradi. Hamkasblaringiz bilan raqobat qilishda ishtirok eting, o‘z reytingingiz va reputatsiyangizni, shu bilan birgalikda, mijozlaringiz sonini oshiring.",
+	'ЗАДАТЬ ВОПРОС ЮРИСТАМ' => "Savol berish",
+	'ЗАРЕГИСТРИРОВАТЬСЯ' => "Ro'yxatdan o'tish",
+];
