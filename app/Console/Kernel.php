3c015c293fef9da164619e0f12f1147d0e52dc54
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-09-05 17:26:46 +0500

    Laravel default

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->daily()->at('13:00');
-        $schedule->command('backup:run')->daily()->at('13:01');
+        $schedule->command('backup:clean')->dailyAt('17:27');
+        $schedule->command('backup:run')->dailyAt('17:28');
         // $schedule->command('inspire')
         //          ->hourly();
     }
--- resources/lang/uz/passwords.php
+++ resources/lang/uz/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => "Parollar kamida olti harfdan iborat bo'lishi kerak va tasdiqlash bilan mos bo'lishi kerak.",
+    'reset' => 'Parolingiz qayta tiklandi!',
+    'sent' => 'Biz parolni tiklash uchun elektron pochta orqali habar yubordik!',
+    'token' => "Ushbu parolni qayta tiklash belgisi noto'g'ri.",
+    'user' => "Ushbu e-mail manzili bo'lgan foydalanuvchini topa olmadik.",
 
 ];
