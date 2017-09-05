d7399ba7339ce51ece8e7e8cb5c1f482f6b537ae
Erkin <erkinboy.botirov@mail.ru>
2017-09-05 14:05:22 +0500

    translation

---
+++ resources/lang/uz/lawyer-dashboard.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erkin
+ * Date: 05.09.2017
+ * Time: 11:30
+ */
+return [
+    'Главная' => "Bosh sahifa",
+    'Юристы' => "Yuristlar",
+    'Вопросы' => "Savollar",
+    'Блог' => "Maqola",
+    'Как это работает' => "Bu qanday ishlaydi",
+    'О нас' => "Jamoa haqida",
+    'Редактировать' => "O‘zgartirish",
+    'Как считается рейтинг?' => "Reyting qanday baholanadi?",
+    'Отзывы' => "Izohlar",
+    'положительных' => "ijobiy",
+    'отрицательных' => "salbiy",
+    'Мой аккаунт' => "Mening akkauntim",
+    'юркоинов' => "yurkoin",
+    'Управление балансом' => "Hisobni boshqarish",
+    'Моя практика' => "Mening amaliyotim",
+    'Написать Блог' => "Maqola yozish",
+    'Документы' => "Hujjatlar",
+    'Телефонные консультации' => "Telefon konsultatsiyalar",
+    'Отвечайте на вопросы клиентов – зарабатывайте деньги и рейтинг' => "Mijozlarning savollariga javob bering - mablag‘ va reyting ishlab oling.",
+    'Перейти к вопросам »' => "Savollarga o‘tish »",
+    'Консультируйте клиентов по телефону, используя нашу систему' => "Ushbu sistemadan foydalanib, mijozlarga telefon orali konsultatsiya xizmatini ko‘rsating.",
+    'Перейти к заявкам »' => "Xizmatga o‘tish »",
+    'Готовьте различные юридические документы для наших клиентов' => "Mijozlar uchun turli huquqiy hujjatlarni tayyorlab bering.",
+    'Перейти к документам »' => "Hujjatlarga o‘tish »",
+    'Заявки' => "Arizalar",
+    'Приобретайте заявки (вопросы и контакты клиента) по необходимым параметрам' => "Mijozlarning ehtiyojlariga asosan arizalar (savollar va mijoz kontaktlari) ni qabul qiling.",
+    'Моя юридическая практика' => "Yuridik amaliyotim",
+    'Консультации по телефону' => "Telefon konsultatsiyalar",
+    'Блоги' => "Maqolalar",
+    'Изменить' => "Изменить",
+    'Мой профиль' => "Mening profilim",
+    'г' => "sh",
+    'сум' => "sum",
+    ];
--- resources/views/lawyer/dashboard.blade.php
+++ resources/views/lawyer/dashboard.blade.php
@@ -4,12 +4,12 @@
     <link href="{{ asset('dist/css/lawyers.css')}}" rel="stylesheet">
 @endsection
 @section('menu')
-  <li><a href="{{ route('home')}}">Главная</a></li>
-  <li><a href="{{ route('lawyers.list')}}">Юристы</a></li>
-  <li><a href="{{ route('question.list')}}">Вопросы</a></li>
-  <li><a href="{{ route('web.blogs')}}">Блог</a></li>
-  <li><a href="{{ route('how-works')}}">Как это работает</a></li>
-  <li><a href="{{ route('about')}}">О нас</a></li>
+    <li><a href="{{ route('home')}}">@lang('lawyer-dashboard.Главная')</a></li>
+    <li><a href="{{ route('lawyers.list')}}">@lang('lawyer-dashboard.Юристы')</a></li>
+    <li><a href="{{ route('question.list')}}">@lang('lawyer-dashboard.Вопросы')</a></li>
+    <li><a href="{{ route('web.blogs')}}">@lang('lawyer-dashboard.Блог')</a></li>
+    <li><a href="{{ route('how-works')}}">@lang('lawyer-dashboard.Как это работает')</a></li>
+    <li><a href="{{ route('about')}}">@lang('lawyer-dashboard.О нас')</a></li>
 @endsection
 @section('content')
 
@@ -21,7 +21,7 @@
                 <div class="panel-heading">
                     <h4 class="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#profile">
-                            Мой профиль <b class="caret"></b></a>
+                            @lang('lawyer-dashboard.Мой профиль') <b class="caret"></b></a>
                     </h4>
                 </div>
                 <div id="profile" class="panel-collapse collapse in">
@@ -29,27 +29,27 @@
                         <ul class="list-unstyled">
                             <li>
                                 <h6><b>{{$lawyer->user->firstName}} {{$lawyer->user->lastName}}</b></h6>
-                                <p class="color-gray">г. {{$lawyer->user->city->name}}</p>
-                                <a href="{{ route('lawyer.info')}}">Редактировать</a>
+                                <p class="color-gray">@lang('lawyer-dashboard.г'). {{$lawyer->user->city->name}}</p>
+                                <a href="{{ route('lawyer.info')}}">@lang('lawyer-dashboard.Редактировать')</a>
                             </li>
                             <li>
                                 <i class="fa fa-hourglass pull-left"></i>
                                 <p class="color-grey">
-                                    Как считается рейтинг?
+                                    @lang('lawyer-dashboard.Как считается рейтинг?')
                                 </p>
                                 <p>
-                                    <a href="#">Как считается рейтинг?</a>
+                                    <a href="#">@lang('lawyer-dashboard.Как считается рейтинг?')</a>
                                 </p>
                             </li>
                             <li>
                                 <p>
-                                    <a href="#">Отзывы:</a>
+                                    <a href="#">@lang('lawyer-dashboard.Отзывы'):</a>
                                 </p>
                                 <p>
-                                   {{$lawyer->countPositiveFeedbacks()}} положительных
+                                   {{$lawyer->countPositiveFeedbacks()}} @lang('lawyer-dashboard.положительных')
                                 </p>
                                 <p>
-                                   {{$lawyer->countNegativeFeedbacks()}} отрицательных
+                                   {{$lawyer->countNegativeFeedbacks()}} @lang('lawyer-dashboard.отрицательных')
                                 </p>
                             </li>
                         </ul>
@@ -63,16 +63,16 @@
                 <div class="panel-heading">
                     <h4 class="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#account">
-                            Мой аккаунт <b class="caret"></b></a>
+                            @lang('lawyer-dashboard.Мой аккаунт') <b class="caret"></b></a>
                     </h4>
                 </div>
                 <div id="account" class="panel-collapse collapse in">
                     <div class="panel-body">
                         <ul class="list-unstyled">
                             <li>
-                                <h3>{{$lawyer->user->balance()}} сум.</h3>
-                                <h3>0 юркоинов</h3>
-                                <a href="{{route('card.payment')}}">Управление балансом</a>
+                                <h3>{{$lawyer->user->balance()}} @lang('lawyer-dashboard.сум').</h3>
+                                <h3>0 @lang('lawyer-dashboard.юркоинов')</h3>
+                                <a href="#">@lang('lawyer-dashboard.Управление балансом')</a>
                             </li>
                         </ul>
                     </div>
@@ -85,24 +85,24 @@
                 <div class="panel-heading">
                     <h4 class="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#services">
-                            Моя практика <b class="caret"></b></a>
+                            @lang('lawyer-dashboard.Моя практика') <b class="caret"></b></a>
                     </h4>
                 </div>
                 <div id="services" class="panel-collapse collapse in">
                     <div class="panel-body">
                         <ul class="list-unstyled">
                             <li>
-                                <a href="#">Вопросы</a>
+                                <a href="#">@lang('lawyer-dashboard.Вопросы')</a>
                             </li>
 
                             <li>
-                                <a href="{{route('lawyer.blog.insert')}}">Написать Блог</a>
+                                <a href="{{route('lawyer.blog.insert')}}">@lang('lawyer-dashboard.Написать Блог')</a>
                             </li>
                             <li>
-                                <a href="#">Документы</a>
+                                <a href="#">@lang('lawyer-dashboard.Документы')</a>
                             </li>
                             <li>
-                                <a href="#">Телефонные консультации</a>
+                                <a href="#">@lang('lawyer-dashboard.Телефонные консультации')</a>
                             </li>
                         </ul>
                     </div>
@@ -114,49 +114,45 @@
             <div class="row text-center background-white border-gray" id="services-list">
                 <div class="row">
                     <div class="col-sm-6">
-                        <h4 class="text-success">Вопросы</h4>
-                        <p>Отвечайте на вопросы клиентов –
-                            зарабатывайте деньги и рейтинг</p>
-                        <a type="button" href="{{route('question.list')}}" class="btn btn-default btn-success">Перейти к вопросам »</a>
+                        <h4 class="text-success">@lang('lawyer-dashboard.Вопросы')</h4>
+                        <p>@lang('lawyer-dashboard.Отвечайте на вопросы клиентов – зарабатывайте деньги и рейтинг')</p>
+                        <a type="button" href="{{route('question.list')}}" class="btn btn-default btn-success">@lang('lawyer-dashboard.Перейти к вопросам »')</a>
                     </div>
                     <div class="col-sm-6">
-                        <h4 class="text-primary">Телефонные консультации</h4>
-                        <p>Консультируйте клиентов по телефону,
-                            используя нашу систему</p>
-                        <button type="button" class="btn btn-default btn-primary">Перейти к заявкам »</button>
+                        <h4 class="text-primary">@lang('lawyer-dashboard.Телефонные консультации')</h4>
+                        <p>@lang('lawyer-dashboard.Консультируйте клиентов по телефону, используя нашу систему')</p>
+                        <button type="button" class="btn btn-default btn-primary">@lang('lawyer-dashboard.Перейти к заявкам »')</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-6">
-                        <h4 class="text-warning">Документы</h4>
-                        <p>Готовьте различные юридические
-                            документы для наших клиентов</p>
+                        <h4 class="text-warning">@lang('lawyer-dashboard.Документы')</h4>
+                        <p>@lang('lawyer-dashboard.Готовьте различные юридические документы для наших клиентов')</p>
                         <a type="button" href="{{ route
-                        ('lawyer.document.info')}}" class="btn btn-default btn-warning">Перейти к документам »</a>
+                        ('lawyer.document.info')}}" class="btn btn-default btn-warning">@lang('lawyer-dashboard.Перейти к документам »')</a>
                     </div>
                     <div class="col-sm-6">
-                        <h4 class="text-danger">Заявки</h4>
-                        <p>Приобретайте заявки (вопросы и контакты
-                            клиента) по необходимым параметрам</p>
-                        <button type="button" class="btn btn-default btn-danger">Перейти к заявкам »</button>
+                        <h4 class="text-danger">@lang('lawyer-dashboard.Заявки')</h4>
+                        <p>@lang('lawyer-dashboard.Приобретайте заявки (вопросы и контакты клиента) по необходимым параметрам')</p>
+                        <button type="button" class="btn btn-default btn-danger">@lang('lawyer-dashboard.Перейти к заявкам »')</button>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-12 border-gray background-white" id="orders">
-                    <h5 class="text-success">Моя юридическая практика</h5>
+                    <h5 class="text-success">@lang('lawyer-dashboard.Моя юридическая практика')</h5>
                     <ul class="nav nav-tabs">
                         <li class="active">
-                            <a data-toggle="tab" href="#answered-questions">Вопросы</a>
+                            <a data-toggle="tab" href="#answered-questions">@lang('lawyer-dashboard.Вопросы')</a>
                         </li>
                         <li>
-                            <a data-toggle="tab" href="#call-consultions">Консультации по телефону</a>
+                            <a data-toggle="tab" href="#call-consultions">@lang('lawyer-dashboard.Консультации по телефону')</a>
                         </li>
                         <li>
-                            <a data-toggle="tab" href="#document-requests">Документы</a>
+                            <a data-toggle="tab" href="#document-requests">@lang('lawyer-dashboard.Документы')</a>
                         </li>
                         <li>
-                            <a data-toggle="tab" href="#blog-created">Блоги</a>
+                            <a data-toggle="tab" href="#blog-created">@lang('lawyer-dashboard.Блоги')</a>
                         </li>
                     </ul>
 
@@ -196,7 +192,7 @@
                                     <ul>
                                         @foreach($lawyer->blogs as $blog)
                                             <li><a href="{{ route('web.blog.show',$blog->id)}}"> {{$blog->title}}</a>
-                                                <a class="btn btn-info btn-xs" href="{{route('lawyer.blog.edit', $blog->id)}}">Изменить</a></li>
+                                                <a class="btn btn-info btn-xs" href="{{route('lawyer.blog.edit', $blog->id)}}">@lang('lawyer-dashboard.Изменить')</a></li>
                                         @endforeach
                                     </ul>
                                 </div>
