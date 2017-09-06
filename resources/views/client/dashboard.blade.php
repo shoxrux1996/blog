dddcc20efff46ab5120b050198bf7363e69a7927
Erkin <erkinboy.botirov@mail.ru>
2017-09-06 13:50:12 +0500

    translation

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -3,12 +3,12 @@
     <link href="{{ asset('dist/css/client.css')}}" rel="stylesheet">
 @endsection
 @section('menu')
-    <li><a href="{{ route('home')}}">Главная</a></li>
-    <li><a href="{{ route('lawyers.list')}}">Юристы</a></li>
-    <li><a href="{{ route('question.list')}}">Вопросы</a></li>
-    <li><a href="{{ route('web.blogs')}}">Блог</a></li>
-    <li><a href="{{ route('how-works')}}">Как это работает</a></li>
-    <li><a href="{{ route('about')}}">О нас</a></li>
+    <li><a href="{{ route('home')}}">@lang('Главная')</a></li>
+    <li><a href="{{ route('lawyers.list')}}">@lang('Юристы')</a></li>
+    <li><a href="{{ route('question.list')}}">@lang('Вопросы')</a></li>
+    <li><a href="{{ route('web.blogs')}}">@lang('Блог')</a></li>
+    <li><a href="{{ route('how-works')}}">@lang('Как это работает')</a></li>
+    <li><a href="{{ route('about')}}">@lang('О нас')</a></li>
 @endsection
 @section('content')
 <!-- Modal for message-->
@@ -84,7 +84,7 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
                             <a data-toggle="collapse" data-parent="#accordion" href="#profile">
-                                Мой профиль <b class="caret"></b></a>
+                                @lang('Мой профиль') <b class="caret"></b></a>
                         </h4>
                     </div>
                     <div id="profile" class="panel-collapse collapse in">
@@ -93,11 +93,11 @@
                                 <li>
                                     <h6><b>{{$client->user->firstName}} {{$client->user->lastName}}</b></h6>
                                     <p class="color-gray">г. {{$client->user->city->name}}</p>
-                                    <a href="{{ route('client.info')}}">Редактировать</a>
+                                    <a href="{{ route('client.info')}}">@lang('Редактировать')</a>
                                 </li>
                                 <li>
-                                    <h3>{{$client->user->balance()}} сум.</h3>
-                                    <a href="{{route('card.payment')}}">Управление балансом</a>
+                                    <h3>{{$client->user->balance()}} @lang('сум').</h3>
+                                    <a href="#">@lang('Управление балансом')</a>
                                 </li>
                             </ul>
                         </div>
@@ -110,20 +110,20 @@
                     <div class="panel-heading">
                         <h4 class="panel-title">
                             <a data-toggle="collapse" data-parent="#accordion" href="#services">
-                                Мой профиль <b class="caret"></b></a>
+                                @lang('Мой профиль') <b class="caret"></b></a>
                         </h4>
                     </div>
                     <div id="services" class="panel-collapse collapse in">
                         <div class="panel-body">
                             <ul class="list-unstyled">
                                 <li>
-                                    <a href="{{ route('my.questions')}}">Вопросы юристам</a>
+                                    <a href="{{ route('my.questions')}}">@lang('Вопросы юристам')</a>
                                 </li>
                                 <li>
-                                    <a href="#">Консультации по телефону</a>
+                                    <a href="#">@lang('Консультации по телефону')</a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('my.documents')}}">Документы</a>
+                                    <a href="#">@lang('Документы')</a>
                                 </li>
                             </ul>
                         </div>
@@ -134,37 +134,33 @@
             <div class="col-sm-9">
                 <div class="row text-center background-white border-gray" id="services-list">
                     <div class="col-sm-4">
-                        <h4 class="text-success">Вопрос</h4>
-                        <p>Задайте любой вопрос, и в течение 15 минут вы получите ответы наших юристов.</p>
-                        <a href="{{ route('question.create') }}" type="button" class="btn btn-default btn-success">Задать
-                            вопрос</a>
+                        <h4 class="text-success">@lang('Вопрос')</h4>
+                        <p>@lang('Задайте любой вопрос, и в течение 15 минут вы получите ответы наших юристов.')</p>
+                        <a href="{{ route('question.create') }}" type="button" class="btn btn-default btn-success">@lang('Задать вопрос')</a>
                     </div>
                     <div class="col-sm-4">
-                        <h4 class="text-primary">Звонок</h4>
-                        <p>Оставьте номер телефона, и наш юрист свяжется с вами, чтобы проконсультировать вас по любому
-                            вопросу.</p>
-                        <a data-toggle="modal" data-target="#call-function" type="button" class="btn btn-default btn-primary">Заказать
-                            звонок</a>
+                        <h4 class="text-primary">@lang('Звонок')</h4>
+                        <p>@lang('Оставьте номер телефона, и наш юрист свяжется с вами, чтобы проконсультировать вас по любому вопросу.')</p>
+                        <a data-toggle="modal" data-target="#call-function" type="button" class="btn btn-default btn-primary">@lang('Заказать звонок')</a>
                     </div>
                     <div class="col-sm-4">
-                        <h4 class="text-warning">Документ</h4>
-                        <p>Закажите документ, после чего наш юрист свяжется с вами, уточнит детали и подготовит его.</p>
-                        <a data-toggle="modal" data-target="#document-function" type="button" class="btn btn-default btn-warning">Заказать
-                            документ</a>
+                        <h4 class="text-warning">@lang('Документ')</h4>
+                        <p>@lang('Закажите документ, после чего наш юрист свяжется с вами, уточнит детали и подготовит его.')</p>
+                        <a data-toggle="modal" data-target="#document-function" type="button" class="btn btn-default btn-warning">@lang('Заказать документ')</a>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-12 border-gray background-white" id="orders">
-                        <h5 class="text-success">Мои заказы</h5>
+                        <h5 class="text-success">@lang('Мои заказы')</h5>
                         <ul class="nav nav-tabs">
                             <li class="active">
-                                <a data-toggle="tab" href="#asked-questions">Вопросы юристам</a>
+                                <a data-toggle="tab" href="#asked-questions">@lang('Вопросы юристам')</a>
                             </li>
                             <li>
-                                <a data-toggle="tab" href="#call-consultions">Консультации по телефону</a>
+                                <a data-toggle="tab" href="#call-consultions">@lang('Консультации по телефону')</a>
                             </li>
                             <li>
-                                <a data-toggle="tab" href="#document-requests">Документы</a>
+                                <a data-toggle="tab" href="#document-requests">@lang('Документы')</a>
                             </li>
                             <li>
                                 <a data-toggle="tab" href="#notifications"><i class="fa fa-bell"
@@ -219,7 +215,7 @@
                                         </ul>
                                         <div >
                                             <a href="{{route('client.notifications.delete')}}"
-                                               class="btn btn-info btn-xs"> Mark as Ready</a>
+                                               class="btn btn-info btn-xs"> @lang('Mark as Ready')</a>
                                         </div>
                                     </div>
                                 </div>
