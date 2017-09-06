eef4effc830f202cb5aca6653d697890ed4b5f67
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-09-07 00:20:15 +0500

    icons telegram ...

--- resources/lang/uz/mail.php
+++ resources/lang/uz/mail.php
@@ -12,4 +12,8 @@ return [
     'Активация'=> 'Faollashtirish',
     'Привет' => 'Assalomu alaykum',
     'Телефон'=>'Telefon',
+    'email_confirm' => "Elektron manzilingizni tasdiqlang",
+    'email_confirm_div' => "Yuridik.uz da ro'yxatdan o'tkazganingiz uchun tashakkur.
+E-pochta manzilingizni tasdiqlash uchun quyidagi linkni bosing.",
+    'confirm_email_button'=> 'Emailni Tasdiqlang',
 ];
