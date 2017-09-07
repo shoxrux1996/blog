daf38ec13e352b13de312a79a50c3cd706f9609a
Erkin <erkinboy.botirov@mail.ru>
2017-09-07 11:49:56 +0500

    translation

---
+++ resources/lang/uz/guarantees.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erkin
+ * Date: 07.09.2017
+ * Time: 11:13
+ */
+    return [
+        'Главная' => "Bosh sahifa",
+        'Юристы' => "Yuristlar",
+        'Вопросы' => "Savollar",
+        'Блог' => "Maqolalar",
+        'Как это работает' => "Bu qanday ishlaydi",
+        'О нас' => "Loyiha haqimida",
+        'Политика защиты клиентов Yuridik.uz' => "Yuridik.uz mijozlari huquqlarini himoya qilish Siyosati",
+        'Yuridik.uz предоставляет своим клиентам широкие гарантии качества, конфиденциальности и безопасности. На нашем сайте вы можете получить юридическую помощь по всем отраслям права, не беспокоясь о репутации юристов, безопасности платежей и ваших персональных данных - мы уже сделали все это за вас.' => "Yuridik.uz o’z mijozlariga sifati kafolatlangan, shaxsiy ma’lumotlarning maxfiyligini saqlagan holda va xavfsiz tarzda onlayn yuridik xizmatlarni taqdim etadi. Saytimiz orqali Siz huquqning istalgan sohasi bo’yicha  istalgan vaqtda savolingizga professional yuristlardan javob olishingiz mumkin. Xizmatlardan foydalanyotganingizda sizning shaxsiy ma’lumotlaringiz xavfsizligini kafolatlaymiz.",
+        'Лучшие из лучших' => "Eng yaxshi huquqshunoslar",
+        'Наш сервис объединяет лучших специалистов со всей России, чтобы качественная юридическая консультация была доступна для вас в любое время.' => "Bizning portalimiz sifatli va hamyonbop yuridik xizmatlar barcha uchun ochiq bo’lishini ta’minlash maqsadida butun O’zbekiston bo’ylab eng yaxshi huquqshunoslarni birlashtiradi.",
+        'Все консультирующие юристы прошли процедуру подтверждения высшего образования и опыта работы.' => "Loyihada ishtirok etuvchi barcha huquqshunoslarning yurisprudensiya sohasida oliy ma’lumotli ekanligi va yetarlicha tajribagaliligi tekshirilgan.",
+        'Возврат денег' => "Pulni qaytarish",
+        'Если вы считаете, что юрист не помог вам, мы вернем вам деньги. Просто напишите нам об этом в течение 30 дней.' => "Agarda siz huquqshunoslar yordam bermagan va/yoki savolingizga yetarlicha javob bermagan deb hisoblasangiz, biz sizga pulingizni qaytaramiz. Pulingizni qaytarish uchun savol berilgan sanadan boshlab 30 kun ichida bizga yozing. Shikoyatlarni qabul qilish uchun email: appeal@yuridik.uz  ",
+        'Конфиденциальность' => "Shaxsiy ma’lumotlar maxfiyligi",
+        'Мы ценим ваше доверие. Данные, указанные при регистрации и в процессе работы с сервисом, не публикуются в открытом доступе.' => "Sizning ishonchingizni oqlash faoliyatimizning bosh tamoyillaridan biridir. Ro`yxatdan o’tish jarayonida va loyihadan foydalanishning boshqa bosqichlarida kiritiladigan shaxsiy ma’lumotlar hech qayerda chop etilmaydi",
+        'Контактные данные, включая номер телефона, могут быть доступны нашим юристам для уточнения деталей или оказания консультаций.' => "Aloqa ma’lumotlaridan (telefon raqami, email manzili) ma’muriyat tomonidan sizga bog’lanish va/yoki huquqshunoslar tomonidan savolning to’liq mazmunini aniqlashtirish uchun foydalanilishi mumkin.",
+        'Доверьте нам решение проблем!' => "Muammoli masalani hal qilishni bizga ishoning!",
+        'Задать вопрос юристу' => "Huquqshunosga savol berish",
+        'Поддержка' => "Qo’llab-quvvatlash",
+        'Если у вас возникли вопросы по работе с нашим сервисом, то мы с радостью поможем вам во всем разобраться. Просто' => "Agarda sizda loyiha faoliyati bilan bog’liq savol, taklif yoki shikoyat bo’lsa, biz sizni barcha imkoniyatarimizdan foydalanib qo’llab-quvvatlashga va’da beramiz. Buning uchun bizga",
+        'напишите нам' => " xabar qoldiring",
+        'Обратите, пожалуйста, внимание, что служба технической поддержки не оказывает консультаций по юридическим вопросам.' => "ESLATMA! Texnik qo’llab-quvvatlash xizmati yuridik masalalar bo’yicha yuridik konsultatsiya ko’rsatmaydi, yuridik savollarni maxsus bo’limga yozing.",
+        'Дорогие друзья' => "Hurmatli foydalanuvchilar",
+        'Мы делаем все, чтобы качество наших услуг было на самом высоком уровне, а вы чувствовали, что в любой момент вам есть к кому обратиться.' => "Sizga yuqori sifatda xizmat ko’rsatish uchun barcha imkoniyatlarimizni ishga solamiz! Agarda qandaydir shikoyatingiz, taklifingiz bo’lsa, har qanday vaqtda menga",
+        'Напишите мне' => " xat yozishingiz mumkin.",
+        'поделитесь своим мнением о работе сервиса и идеями по его улучшению. Мы ценим ваш выбор и гордимся оказанным нам доверием.' => "Loyiha bo’yicha fikringizni bildiring, yanada yaxshilash uchun g’oyalar bering. Biz sizning fikringizni va ishonchingizni qardlaymiz! Sizning tanlovingiz va bildirgan ishonchingizdan faxrlanamiz!",
+        'С уважением, руководитель Yuridik.uz' => "Hurmat bilan, Yuridik.uz loyihasi koordinatori",
+    ];
