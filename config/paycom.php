acb898f68e4944627064d7b24d28c7b4de20ad03
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-09-30 12:07:31 +0500

    Paycom Test

--- config/paycom.php
+++ config/paycom.php
@@ -2,13 +2,13 @@
 // sample configuration with fake values
 return [
     // Get it in merchant's cabinet in cashbox settings
-    'merchant_id' => '59919d56e8ce8437a1967973',
+    'merchant_id' => '59cf3ff8e8ce8437a1967afb',
 
     // Login is always "Paycom"
     'login' => 'Paycom',
 
     // File with cashbox key (key can be found in cashbox settings)
-    'keyFile' => 'jtE2oJ3ZfXDQCsy6NcS@nZD4n?yXFgO6iBNn',
+    'keyFile' => '8Ch&AeHQtTjn6bjTwq085QEZ3O5r1Zdp%Di8',
     // Your database settings
    
 ];
